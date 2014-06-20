1b8dcd184ab75bd329da0d18e5146360faae98d0
JaHIY <jaklsy@gmail.com>
2014-06-21 00:16:59 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# check-traffic-card-balance
+# 检查上海交通卡余额的 perl 脚本
+
+``` bash
+$ ./main.pl xxxxxxxxxxx
+截至2012年12月21日，您卡内余额为0元。
+```
+`xxxxxxxxxxx` 此处为11位数字。
---
+++ main.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use autodie;
+use 5.010;
+
+use LWP;
+use Encode qw/encode decode/;
+
+sub get_html($) {
+    my ($card_id) = @_;
+    my $ua = LWP::UserAgent->new;
+    $ua->agent('Mozilla/5.0 (X11; Linux x86_64; rv:33.0) Gecko/20100101 Firefox/33.0');
+    push @{ $ua->requests_redirectable }, 'POST';
+    $ua->cookie_jar({});
+    my $url = 'http://jtk.sptcc.com:8080/servlet';
+    my $response = $ua->post($url, [
+        'Card_id' => $card_id,
+        'hiddentype' => 's_index',
+        ]);
+
+    return encode 'utf8', decode('euc-cn', $response->content);
+
+}
+
+sub main(@) {
+    my ($card_id) = @_;
+    die '请输入您的交通卡卡号（11位数字）！' if (@_ != 1);
+    die '交通卡卡号必须为11位数字！' if !($card_id =~ /[[:digit:]]{11}/);
+    my %card;
+    my $html = &get_html($card_id);
+    if ($html =~ /<span class="greenBoldTitle">  ([[:digit:]]{4})年 ([[:digit:]]{2})月 ([[:digit:]]{2})日  <\/span>/) {
+        $card{'date'} = [$1, $2, $3];
+    }
+
+    if ($html =~ /<span class="orangeNumber">  ([[:digit:].]+)<\/span> <span class="black14"> 元<\/SPAN><\/TD>/) {
+        $card{'money'} = $1;
+    }
+
+    print "截至${card{'date'}[0]}年${card{'date'}[1]}月${card{'date'}[2]}日，您卡内余额为${card{'money'}}元。\n";
+}
+
+&main(@ARGV);
