c358b23095982aad8642cea4cbd2111264b419f5
JaHIY <jaklsy@gmail.com>
2014-06-21 00:43:24 +0800

    delete &

--- main.pl
+++ main.pl
@@ -29,7 +29,7 @@ sub main(@) {
     die '请输入您的交通卡卡号（11位数字）！' if (@_ != 1);
     die '交通卡卡号必须为11位数字！' if !($card_id =~ /[[:digit:]]{11}/);
     my %card;
-    my $html = &get_html($card_id);
+    my $html = get_html $card_id;
     if ($html =~ /<span class="greenBoldTitle">  ([[:digit:]]{4})年 ([[:digit:]]{2})月 ([[:digit:]]{2})日  <\/span>/) {
         $card{'date'} = [$1, $2, $3];
     }
@@ -41,4 +41,4 @@ sub main(@) {
     print "截至${card{'date'}[0]}年${card{'date'}[1]}月${card{'date'}[2]}日，您卡内余额为${card{'money'}}元。\n";
 }
 
-&main(@ARGV);
+main @ARGV;
